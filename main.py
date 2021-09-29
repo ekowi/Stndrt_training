0f090292d9854369a2e8c625cc8124eedbea66e0
ekowi <greget103@gmail.com>
2021-09-29 17:37:59 +0700

    menambahkan comment

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+"""
+Ini adalah comment, digunakan oleh developer untuk memberitahu developer lain tentang kode yang dibuatnya,
+kode ini adalah hello world
+"""
 print("Hello World!")
 print("My Name Eko")
