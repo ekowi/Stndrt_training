b64383153a5b505317d54c1a7cbc3746425e341e
ekowi <greget103@gmail.com>
2021-09-29 22:09:15 +0700

    menambahkan if_else.py

---
+++ if_else.py
@@ -0,0 +1,11 @@
+#Sekuansi: eksekusi berutrutan
+print('Ibu berkata ke budi "Pergi ketoko"')
+print('budi menjawab "baiklah buk"')
+print('budi telah sampai ditoko')
+print('Apakah ada Susu?')
+#Peercabangan: eksekusi terpilih (if else)
+adaSusu = false
+if adaSusu:
+    print("Ada silahkan dibeli")
+else:
+    print("Sudah habis")
