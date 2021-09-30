1319aa50801d74e09ac1807f5c3bd4f9b7777d19
ekowi <greget103@gmail.com>
2021-09-30 13:43:32 +0700

    menambahkan looping.py

--- if_else.py
+++ if_else.py
@@ -1,13 +1,36 @@
-#Sekuansi: eksekusi berutrutan
+"""
+This file for if else basic coding and i use example "mom told me to buy a bottle of milk and 6 eggs"
+"""
+
+# Sekuansi: eksekusi berutrutan
 print('Ibu berkata ke budi "Pergi ketoko"')
 print('budi menjawab "baiklah buk"')
 print('budi telah sampai ditoko')
 print('Apakah ada Susu?')
-#Peercabangan: eksekusi terpilih (if else)
+
+# Peercabangan: eksekusi terpilih (if else)
 adaSusu = True
 if adaSusu:
     print("Ada silahkan dibeli")
+    uang = 10000
+    if uang > 6000:
+        print("Uang cukup, beli susu satu botol")
+    else:
+        print("Uang tidak cukup")
 else:
     print("Sudah habis")
 print("Apakah ada telur?")
 
+adaTelur = False
+if adaTelur:
+    print("Sudah habis")
+else:
+    print("Ada, ingin berapa butir?")
+
+print("ingin 6 butir")
+
+# Perulangan: eksekusi berulang
+jumlah_telur = 6
+
+for jumlah_telur in range(1, jumlah_telur+1):
+    print(f'Telur ke{jumlah_telur}')
---
+++ looping.py
@@ -0,0 +1,11 @@
+"""
+this file will sperate with if_else.py.
+"""
+
+# Pengulangan (looping)
+kucing = 5
+
+for kucing in range(10 < kucing):
+    print("work")
+else:
+    print("not work")
