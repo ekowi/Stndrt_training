b3ee376a9b1eae5bd3ca3dff914352df56dde38b
ekowi <greget103@gmail.com>
2021-10-06 23:08:28 +0700

    menambahkan list.py

---
+++ Belajar Python/if_else_mendalam_2.py
@@ -0,0 +1,10 @@
+# Lanjutand ari if else mendalam 1
+
+sim = int(input("Berapa umur kamu?"))
+belum = 18 - sim
+
+if sim >= 18:
+    print("kamu sudah bisa membuat SIM, Karna umur kamu ", sim)
+
+else:
+    print("kamu belum bisa membuat SIM, tunggu ", belum, "tahun lagi")
---
+++ Belajar Python/if_else_mendalam_3.py
@@ -0,0 +1,21 @@
+# Lanjutan dari if else mendalam
+
+nilai = int(input("Tolong beri bintang pelayan kami:[1,2,3,4,5]"))
+
+if nilai == 1:
+    print("Terima kasih atas penilaiannya, kirim masukan dan saran karna anda memberi bintang 1")
+
+elif nilai == 2:
+    print("Terima kasih atas penilaiannya, jika berkenan tolong berikan saran agar pelayan kami mendapat bintang lebih")
+
+elif nilai == 3:
+    print("Terima kasih atas penilaiannya, kami akan berusaha agar memenuhi kepuasan anda")
+
+elif nilai == 4:
+    print("terima kasih atas penilaiannya, kami akan memberikan anda diskon atas penilaian tersebut")
+
+elif nilai == 5:
+    print("Terima kasih atas penilaiannya, anda telah mendapatkan VIP service")
+
+else:
+    print("Tolong pilih antara 1, 2, 3, 4, 5 terima kasih")
