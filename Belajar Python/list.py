4e0fe691030c05814d8e010d7be6194b74611cbb
ekowi <greget103@gmail.com>
2021-10-08 12:38:23 +0700

    menambahkan list.py

--- Belajar Python/list.py
+++ Belajar Python/list.py
@@ -2,18 +2,50 @@
 Now i will learn tipe data list in tjis file
 """
 
-daftar_buku = ['Jaringan Komputer', 'Berfikir Kritis', 'Belajar Python']
+pemilik_loker = ['ridwan','surya','shinta']
 
-print("tampilkan daftar buku")
-print(daftar_buku)
+print("\nTampilkan isi List")
+print(pemilik_loker)
 
-print('\nTampilkan daftar buku dengan for in')
-for buku in daftar_buku:
-    print(buku)
-print('\nTampilkan daftar buku dari index tertentu')
-print("3. ", daftar_buku[2])
-print("1. ", daftar_buku[0])
+print("\nTampilkan daftar list")
+for loker in pemilik_loker:
+    print(loker)
 
-print('\nTampilkan daftar buku for in range')
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
+print("\nTampilkan daftar loker dengan for in range")
+for i in range(0, len(pemilik_loker)):
+    print(pemilik_loker[i])
+
+print("\nTampilkan index tertentu")
+print(pemilik_loker[2])
+
+print("\nMenambahkan isi List")
+pemilik_loker.append('ratu')
+print("Ratu sudah ditambahkan", pemilik_loker)
+
+#print("\nClear list")
+#pemilik_loker.clear()
+#for i in range(0, len(pemilik_loker)):
+#   print(pemilik_loker[i])
+
+print("\nMengambil list dan store list dengan (pop)")
+vip = pemilik_loker.pop(2)
+for i in range(0, len(pemilik_loker)):
+    print(pemilik_loker[i])
+
+print("\nMelihat store list")
+print(vip)
+
+pemilik_loker = ['ridwan','surya','shinta','ratu']
+print("\n", pemilik_loker)
+print("Menstore pop dari belakang dan dengan operasi minus(-)")
+wanita = pemilik_loker.pop()
+for i in range(0, len(pemilik_loker)):
+    print(pemilik_loker[i])
+
+print("\nOperasi minus (-2)")
+pria = pemilik_loker.pop(-2)
+for i in range(0, len(pemilik_loker)):
+    print(pemilik_loker[i])
+
+print("\nMelihat store list")
+print(pria, wanita)
